e7c155b2a5575106f67d0e3d3b366f3aff87bca1
bcobogar <bcobogar@emeal.nttdata.com>
2026-04-21 08:48:50 +0200

    Initial Commit

---
+++ Ejercicio1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ejercicio1
+//
+//  Created by Beatriz Maria Cobo Garcia on 21/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ejercicio1/Ejercicio1App.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio1App.swift
+//  Ejercicio1
+//
+//  Created by Beatriz Maria Cobo Garcia on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
